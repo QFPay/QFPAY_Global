273d6792d7588280515af3ec7f4430824a537ec3
linan0828 <linan0828@live.cn>
2017-05-15 17:29:20 +0800

    Update payment.py

--- Code/Python/payment.py
+++ Code/Python/payment.py
@@ -7,9 +7,7 @@ def payment():
     data['txdtm'] = GetNowTime()
     data['product_name'] = "香港測試："+str(data['out_trade_no'])
     data['txamt'] = '10'
-    data['mchid'] = 'xQj7bVwVq38PvTwqlZ0jMGdMr6'
-    data['sub_openid']='om3Tivln_Z8-ohW4lCiko50h3Svw'
-    # data['auth_code'] = '130105832659498462'
+    data['mchid'] = ''
     headers = {'X-QF-APPCODE': '', 'X-QF-SIGN': make_sign(data, '')}
     ret = requests.post(url, headers=headers, params=data)
     return ret.text
