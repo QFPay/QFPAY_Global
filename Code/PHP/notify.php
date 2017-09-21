2c5069f9a42c94e971cd2b013be21205f71aca70
linan0828 <linan0828@live.cn>
2017-09-21 11:28:44 +0800

    Update notify.php

--- Code/PHP/notify.php
+++ Code/PHP/notify.php
@@ -6,4 +6,25 @@
     file_put_contents("test.txt",$name.":".$value."\n", FILE_APPEND);
    }
   file_put_contents("test.txt",$data."\n", FILE_APPEND);
+  
+  
+  $arr = json_decode($data,true, 512, JSON_BIGINT_AS_STRING);
+  $app_key = ''; //錢台提供的 App Key
+  $fields_string = '';
+  ksort($arr); //字典排序 A-Z 升序台式
+  foreach($arr as $key=>$value) {
+      $fields_string .= $key.'='.$value.'&' ;
+  }
+  
+  $fields_string = substr($fields_string , 0 , strlen($fields_string) - 1);
+  $sign = '';
+  $sign = strtoupper(md5($fields_string . $app_key));
+  
+  if($sign != $qf_sign){
+      echo '签名错误';
+  }else{
+      //业务处理
+      echo 'success';
+  }
+  
 ?>
